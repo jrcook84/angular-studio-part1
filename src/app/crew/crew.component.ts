64a338008d4540ca3a37f292742f6886d331a322
peter <peter_cook@comcast.com>
2022-08-02 12:41:24 -0400

    angular studio part 1 bonus completed

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -5,8 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.css']
 })
-export class CrewComponent implements OnInit {
 
+
+export class CrewComponent implements OnInit {
+  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
   constructor() { }
 
   ngOnInit(): void {
